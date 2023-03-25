b77f2bbbc3a120296a0345d5e96ac4fda491e767
Atharva Jadhav <jatharva2324@gmail.com>
2023-03-25 18:36:14 +0530

    mongodb setup and schema generation

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db");
+const colors = require('colors');
+//load env vars 
+dotenv.config({path: './config/config.env'});
+
+// connect Database
+connectDB();
 
 //Route files
 const bootcamps = require('./routes/bootcamps');
 
-//load env vars 
-dotenv.config({path: './config/config.env'});
 
 const app = express();
-
+app.use(express.json());
 app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,
-    console.log(`Server runnnig in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT,
+    console.log(`Server runnnig in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
     );
+
+process.on('unhandledRejection',(err)=>{
+    console.log(`Error: ${err.message}`.red);
+    server.close(()=>process.exit(1));
+})
